c21e2a97fc69c0505021bd26914e92a523e6e172
rashadjn <146883435+rashadjn@users.noreply.github.com>
2024-01-11 18:16:21 -0500

    Create minesweeper.h

---
+++ minesweeper.h
@@ -0,0 +1,14 @@
+#ifndef MINESWEEPER_H
+#define MINESWEEPER_H
+
+char * create_board(std::size_t x_dim, std::size_t y_dim);
+void clean_board(char * board);
+void print_board(char * board, std::size_t x_dim, std::size_t y_dim);
+void hide_board(char * board, std::size_t x_dim, std::size_t y_dim);
+int mark(char * board, std::size_t x_dim, std::size_t y_dim, std::size_t x_loc, std::size_t y_loc);
+
+void compute_neighbours(char * board, std::size_t x_dim, std::size_t y_dim);
+bool is_game_won(char * board, std::size_t x_dim, std::size_t y_dim);
+int reveal(char * board, std::size_t x_dim, std::size_t y_dim, std::size_t x_loc, std::size_t y_loc);
+
+#endif
